f2a9ab0bc24a7c492f20856f2426d0bb9181b9e0
alivesree <sreeharikg@live.com>
2018-05-30 01:34:38 +0530

    chnges

--- File Copier/frmFileCopier.cs
+++ File Copier/frmFileCopier.cs
@@ -32,7 +32,6 @@ namespace File_Copier
 
         private void txtFolderOrigin_TextChanged(object sender, EventArgs e)
         {
-            pnlSize.Show();
             pnlProgress.Visible = true;
             pnlFileinfo.Hide();
             Application.DoEvents();
@@ -52,12 +51,14 @@ namespace File_Copier
             long size = 0, count = 0;
             size = new DirectoryInfo(txtFolderOrigin.Text).EnumerateFiles("*.*", SearchOption.AllDirectories).Sum(file => file.Length);
             Application.DoEvents();
-            count = new DirectoryInfo(txtFolderOrigin.Text).EnumerateFiles("*.*", SearchOption.AllDirectories).Count() - 1;
+            count = new DirectoryInfo(txtFolderOrigin.Text).EnumerateFiles("*.*", SearchOption.AllDirectories).Count() ;
             this.Invoke((MethodInvoker)delegate
             {
-                lblFiles.Text = (++count).ToString();
+                pnlSize.Show();
+                lblFiles.Text = (count).ToString();
                 Application.DoEvents();
                 lblSize.Text =  (size / (1024 * 1024 )).ToString() + " MB";
+                lblFiles.Tag = count;
                 this.Refresh();
                 pnlProgress.Hide();
             });
@@ -66,7 +67,46 @@ namespace File_Copier
 
         private void btnCopy_Click(object sender, EventArgs e)
         {
+            pnlProgress.Show();
+            pnlFileinfo.Show();
+            prgBar.ProgressBarStyle = ProgressBarStyle.Continuous;
+            prgBar.Minimum = 0;
+            prgBar.Maximum = Convert.ToInt32( lblFiles.Tag);
+            prgBar.Refresh();
+            prgBar.HideProgressText = false;
+            lblStatus.Text = "Copying";
+            foreach (string newPath in Directory.GetFiles(txtFolderOrigin.Text, "*.*", SearchOption.AllDirectories))
+            {
+                try
+                {
+                    int count = 1;
+                    string newFullPath = txtFolderPath.Text + newPath.Split('\\').Last();
+                    while (File.Exists(newFullPath))
+                    {
+                        if (new FileInfo(newPath).Extension != "")
+                        {
+                            string tempFileName = string.Format("{0}({1})", newPath.Split('\\').Last().Replace(new FileInfo(newPath).Extension, ""), count++);
+                            newFullPath = Path.Combine(newFullPath.Replace(newFullPath.Split('\\').Last(), ""), tempFileName + new FileInfo(newPath).Extension);
+                        }
+                        else
+                        {
+                            string tempFileName = string.Format("{0}({1})", newPath, count++);
+                            newFullPath = Path.Combine(newFullPath.Replace(newFullPath.Split('\\').Last(), ""), tempFileName + new FileInfo(newPath).Extension);
+                        }
+                    }
+                    lblFileName.Text = new FileInfo(newPath).Name;
+                    File.Copy(newPath, newFullPath);
+                    prgBar.Value++;
+                    this.Refresh();
+                }
+                catch 
+                {
+                    MessageBox.Show("Missed file " + newPath);
+                }
 
+            }
+            this.Refresh();
+            MessageBox.Show("Completed");
         }
     }
 }
